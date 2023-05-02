e9ede6b0ff02fdb0cbbc66903269f3c0625fd7f2
Belintawati Zelda Br G <99598818+Belintawati@users.noreply.github.com>
2023-05-02 23:29:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 	model = load('model_spam_tfidf_nb.model')
 
 	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
+	app.run(host="localhost", port=5000, debug=False)
 	
 	
 
